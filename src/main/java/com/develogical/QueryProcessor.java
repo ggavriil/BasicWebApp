4f6c06d71747e2ac242473c6db1dfebc21c8305c
Lloyd <you@example.com>
2020-01-21 11:53:32 +0000

    fix errors in build.

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,7 @@
 package com.develogical;
 
+import java.util.Arrays;
+
 public class QueryProcessor {
 
     public String process(String query) {
@@ -12,9 +14,9 @@ public class QueryProcessor {
             return "This is not the summer of 69. It's just 69.";
         }
 	if(query.toLowerCase().contains("largest") && query.toLowerCase().contains("number")) {
-		String[] parts = query.toLower().split(':');
-		String[] parts2 = parts[1].split(',');
-		int max = parts2.stream().map(Integer::valueOf).max(Integer::compare).get();
+		String[] parts = query.toLowerCase().split(":");
+		String[] parts2 = parts[2].split(",");
+		Integer max = Arrays.stream(parts2).map(Integer::valueOf).max(Integer::compare).get();
 		return max.toString();
 	}	
         return "";
